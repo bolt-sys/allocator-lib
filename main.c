d0443e99c909de5988c0f7ffeba963ee7ea27105
Emzudemil <emilglenn@outlook.de>
2023-09-16 19:44:39 +0200

    Add functionality for memory descriptor generation
    The main.c file has been updated with functions to generate Memory Descriptors and Kernel Parameters. These functions will randomly generate parameters for Memory Descriptors and return a pointer to a KERNEL_PARAMETERS structure. This is useful for testing memory handling and allocation. Previous implementation was just printing a 'Hello World!' string which has been replaced with this functionality plus a more detailed output of the generated data.

--- main.c
+++ main.c
@@ -2,9 +2,66 @@
 // Created by valo on 9/15/23.
 //
 #include <stdio.h>
+#include <Kernel.h>
+#include <stdlib.h>
+#include <time.h>
+
+
+MEMORY_DESCRIPTOR** generateMemoryDescriptor()
+{
+	MEMORY_DESCRIPTOR** descriptor = malloc(sizeof(MEMORY_DESCRIPTOR*) * 20);
+	for (int i = 0; i < 20; ++i)
+	{
+		int size = 8192 + (((float )rand() / RAND_MAX) * 32768);
+		descriptor[i] = malloc(sizeof(MEMORY_DESCRIPTOR));
+		descriptor[i]->Base = (UINT64) malloc(size);
+		descriptor[i]->Length = size;
+		descriptor[i]->Type = (((float) rand() / RAND_MAX) * 10) < 6.5 ? MEMMAP_USABLE : MEMMAP_RESERVED;
+	}
+	return descriptor;
+}
+
+KERNEL_PARAMETERS* GenerateParameters(void)
+{
+	KERNEL_PARAMETERS*  Parameters;
+	CHAR8*              CommandLine;
+	MEMORY_DESCRIPTOR** Descriptor;
+
+	srand(time(0));
+
+	Parameters = (KERNEL_PARAMETERS*)malloc(sizeof(KERNEL_PARAMETERS));
+	if (Parameters == NULL) {
+		return NULL;
+	}
+
+	CommandLine = (CHAR8*)malloc(256);
+	if (CommandLine == NULL) {
+		free(Parameters);
+		return NULL;
+	}
+
+	Descriptor = generateMemoryDescriptor();
+
+	Parameters->CommandLineSize = 256;
+	Parameters->CommandLine = &CommandLine;
+	Parameters->MemoryMapSize = 20;
+	Parameters->MemoryMap = Descriptor;
+
+	return Parameters;
+}
 
 int main(void)
 {
-	printf("Hello world!");
+	KERNEL_PARAMETERS* Parameters = GenerateParameters();
+	if (Parameters == NULL) {
+		printf("Failed to generate parameters!");
+		return 1;
+	}
+	// print memory descriptors
+	for (UINTN i = 0; i < Parameters->MemoryMapSize; ++i)
+	{
+		MEMORY_DESCRIPTOR* Descriptor = Parameters->MemoryMap[i];
+		printf("Memory Descriptor %llu: Base: %llu, Length: %llu, Type: %llu\n", i, Parameters->MemoryMap[i]->Base, Parameters->MemoryMap[i]->Length, Parameters->MemoryMap[i]->Type);
+	}
 	return (0);
 }
